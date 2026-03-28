5a29fa67917210e5451eeb23eb9d5f84cb5ea0c1
infinitysbrother6472-ship-it <infinitysbrother6472@gmail.com>
2026-03-29 04:54:28 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Watching over the kilorou den",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
